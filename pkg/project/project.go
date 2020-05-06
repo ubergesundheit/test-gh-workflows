b523f0e58cbcd641a482125b450968680642d01d
Paweł Kopiczko <kopiczko@gmail.com>
2020-05-06 11:11:31 +0100

    release v7.1.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "1.0.9"
+	version            = "7.1.0"
 )
 
 func Description() string {
