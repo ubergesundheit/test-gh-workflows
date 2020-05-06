cf39e8e3786f6c0b96ba6436e12f2ba8cbe0534c
Paweł Kopiczko <kopiczko@gmail.com>
2020-05-06 11:10:50 +0100

    break version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "7.0.9"
+	version            = "1.0.9"
 )
 
 func Description() string {
