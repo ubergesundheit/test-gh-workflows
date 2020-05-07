29d1cd61f16c591dee279b056cc1421b89cd2c29
GitHub Action <action@github.com>
2020-05-07 14:37:37 +0000

    pkg/project: bump version to 10.0.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "10.0.1-pkg"
+	version            = "10.0.2-dev"
 )
 
 func Description() string {
