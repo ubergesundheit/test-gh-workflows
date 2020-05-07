91e9787e24a91b6d29042ca45ed7edac1512c0bb
Paweł Kopiczko <kopiczko@gmail.com>
2020-05-07 15:28:15 +0100

    add pkg

---
+++ pkg/project/project.go
@@ -0,0 +1,29 @@
+package project
+
+var (
+	description        = "The aws-operator manages Kubernetes clusters running on AWS."
+	gitSHA             = "n/a"
+	name        string = "aws-operator"
+	source      string = "https://github.com/giantswarm/aws-operator"
+	version            = "8.4.1-dev"
+)
+
+func Description() string {
+	return description
+}
+
+func GitSHA() string {
+	return gitSHA
+}
+
+func Name() string {
+	return name
+}
+
+func Source() string {
+	return source
+}
+
+func Version() string {
+	return version
+}
